c54e90b08bb43c54c0cb2baa47b5f37a81daee79
Your <mouad.srk8@gmail.com>
2023-03-22 18:36:41 -0700

    sdfgh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
